0d7bce8cd6fc667057199ca850acd387e18f6f53
carlobortolan <carlobortolan@gmail.com>
2022-05-04 00:27:18 +0200

    Backlog Item 11

--- src/de/tum/in/ase/eist/car/FastCar.java
+++ src/de/tum/in/ase/eist/car/FastCar.java
@@ -7,7 +7,7 @@ public class FastCar extends Car {
 	private static final String FAST_CAR_IMAGE_FILE = "KirbyCar.png";
 
 	private static final int MIN_SPEED_FAST_CAR = 2;
-	private static final int MAX_SPEED_FAST_CAR = 15;
+	private static final int MAX_SPEED_FAST_CAR = 10;
 
 	public FastCar(Dimension2D gameBoardSize) {
 		super(gameBoardSize);
