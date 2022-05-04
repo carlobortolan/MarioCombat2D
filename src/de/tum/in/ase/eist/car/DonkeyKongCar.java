22aea89804ba70740642c610cb0b55c641eb7d78
Carlo Bortolan <carlo.bortolan@tum.de>
2022-05-04 11:49:30 +0200

    ets

--- src/de/tum/in/ase/eist/car/DonkeyKongCar.java
+++ src/de/tum/in/ase/eist/car/DonkeyKongCar.java
@@ -4,16 +4,16 @@ import de.tum.in.ase.eist.Dimension2D;
 
 public class DonkeyKongCar extends Car {
 
-    private static final String DonkeyKong_Car_IMAGE_FILE = "DonkeyKongCar.png";
+    private static final String DONKEYKONG_Car_IMAGE_FILE = "DonkeyKongCar.png";
 
-    private static final int MIN_SPEED_DonkeyKong_Car = 2;
-    private static final int MAX_SPEED_DonkeyKong_Car = 6;
+    private static final int MIN_SPEED_DONKEYKONG_Car = 2;
+    private static final int MAX_SPEED_DONKEKONG_Car = 6;
 
     public  DonkeyKongCar(Dimension2D gameBoardSize) {
         super(gameBoardSize);
-        setMinSpeed(MIN_SPEED_DonkeyKong_Car);
-        setMaxSpeed(MAX_SPEED_DonkeyKong_Car);
+        setMinSpeed(MIN_SPEED_DONKEYKONG_Car);
+        setMaxSpeed(MAX_SPEED_DONKEKONG_Car);
         setRandomSpeed();
-        setIconLocation(DonkeyKong_Car_IMAGE_FILE);
+        setIconLocation(DONKEYKONG_Car_IMAGE_FILE);
     }
 }
